730445a7198481336cb9128b799edbc38aaf2d78
BYB Digital <buildyourbranddigital@gmail.com>
2025-10-16 17:03:08 +0000

    asd

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -46,7 +46,7 @@ export function Contact() {
     }
     
     try {
-      const response = await fetch("https://formsubmit.co/buildyourbranddigital@gmail.com", {
+      const response = await fetch("https://formsubmit.co/87724f18c98b3cf1a7fe4a1e3704864d", {
         method: "POST",
         headers: {
           'Content-Type': 'application/json',
